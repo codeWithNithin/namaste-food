803399e446e89a027be5f980cb84e1a9a22300cf
codeWithNithin <nithinkottary25@gmail.com>
2025-09-24 16:07:08 +0530

    day-1: header design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-
   return (
-    <>
-       <h1> Welcome to Namaste Food </h1>
-    </>
-  )
+    <div className="app">
+      <Header />
+    </div>
+  );
 }
 
-export default App
+export default App;
